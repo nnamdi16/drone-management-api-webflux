5233b61592955a5320002b8be29a4119a326dffa
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-04-09 14:46:52 +0200

    chore: test sample class

---
+++ src/test/java/com/nnamdi/gpi/dronemanagementappwebflux/DroneManagementAppWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnamdi.gpi.dronemanagementappwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneManagementAppWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
