1af41ca83aa20c1506ef3716fc90d4f4e6161712
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-04-09 14:52:33 +0200

    chore: implemented an error class to handle error response

---
+++ src/main/java/com/nnamdi/gpi/dronemanagementappwebflux/dto/Error.java
@@ -0,0 +1,18 @@
+package com.nnamdi.gpi.dronemanagementappwebflux.dto;
+
+
+import lombok.Data;
+
+@Data
+public class Error {
+
+    private Integer code;
+    private String message;
+    private String descriptiveMessage;
+
+    public Error(ResponseCodes code, String message, String descriptiveMessage) {
+        this.code = code.code();
+        this.message = message;
+        this.descriptiveMessage = descriptiveMessage;
+    }
+}
