64a6aad3101bc9aa7f0ed3b8950ac2d8896cbe30
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-04-09 14:46:29 +0200

    chore: Application startup class

---
+++ src/main/java/com/nnamdi/gpi/dronemanagementappwebflux/DroneManagementAppWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.nnamdi.gpi.dronemanagementappwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneManagementAppWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneManagementAppWebfluxApplication.class, args);
+	}
+
+}
