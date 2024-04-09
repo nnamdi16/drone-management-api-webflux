3fe8aeee3a74593da51a4e0ce6147168184f5d57
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-04-09 14:53:11 +0200

    chore: implemented ExceptionHandlers to handle various exceptions

---
+++ src/main/java/com/nnamdi/gpi/dronemanagementappwebflux/exception/ExceptionHandlers.java
@@ -0,0 +1,68 @@
+package com.nnamdi.gpi.dronemanagementappwebflux.exception;
+
+
+import com.nnamdi.gpi.dronemanagementappwebflux.dto.Error;
+import com.nnamdi.gpi.dronemanagementappwebflux.dto.Response;
+import com.nnamdi.gpi.dronemanagementappwebflux.dto.ResponseCodes;
+import com.nnamdi.gpi.dronemanagementappwebflux.util.ConstantsUtil;
+import com.nnamdi.gpi.dronemanagementappwebflux.util.ResponseUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionHandlers {
+
+    private final ResponseUtil responseUtil;
+
+    @Autowired
+    public ExceptionHandlers(ResponseUtil responseUtil) {
+        this.responseUtil = responseUtil;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ResponseEntity<Response> handleBadRequest(final MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        List<String> validationErrors = result.getFieldErrors().stream().map(FieldError::getDefaultMessage).toList();
+        String customErrorMessage = "Validation failed. Please check your input.";
+        String errorMessage = customErrorMessage + " " + String.join(", ", validationErrors);
+
+        return ResponseEntity.badRequest().body(responseUtil.getErrorResponse(new Error(ResponseCodes.INVALID_REQUEST, ConstantsUtil.BAD_REQUEST, errorMessage)));
+    }
+
+    @ExceptionHandler(ModelAlreadyExistException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ResponseBody
+    public ResponseEntity<Response> handleMethodAlreadyExist(final ModelAlreadyExistException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(responseUtil.getErrorResponse(new Error(ResponseCodes.INVALID_REQUEST, ConstantsUtil.ALREADY_EXIST, ex.getMessage())));
+    }
+
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ResponseEntity<Response> handleBadRequest(final BadRequestException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseUtil.getErrorResponse(new Error(ResponseCodes.INVALID_REQUEST, ConstantsUtil.BAD_REQUEST, ex.getMessage())));
+    }
+
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ResponseEntity<Response> handleNotFound(final NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseUtil.getErrorResponse(new Error(ResponseCodes.NOT_FOUND, ConstantsUtil.NOT_FOUND, ex.getMessage())));
+    }
+}
