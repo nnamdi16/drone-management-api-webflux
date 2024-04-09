918e10a31ccc692add33d97e467386d789b7c1f4
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-04-09 13:25:45 +0200

    chore: Created a base controller path and resource name

---
+++ src/main/java/com/nnamdi/gpi/dronemanagementappwebflux/controller/BaseApiController.java
@@ -0,0 +1,9 @@
+package com.nnamdi.gpi.dronemanagementappwebflux.controller;
+
+public abstract class BaseApiController {
+    private BaseApiController() {
+    }
+
+    public static final String BASE_API_PATH = "/api/v1/";
+    public static final String DRONE = "drone";
+}
