7503655dba59c8ae94bc8ac7ab3063b216143af3
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-04-09 14:55:31 +0200

    chore: implemented modelMapper configuration

---
+++ src/main/java/com/nnamdi/gpi/dronemanagementappwebflux/config/ModelMapperConfig.java
@@ -0,0 +1,18 @@
+package com.nnamdi.gpi.dronemanagementappwebflux.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setFieldMatchingEnabled(true).setFieldAccessLevel(PRIVATE).setAmbiguityIgnored(true);
+        return modelMapper;
+    }
+}
